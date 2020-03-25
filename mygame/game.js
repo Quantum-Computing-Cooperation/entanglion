fde370bd7a9c0e3100c7979f330c9738df33c661
Ahmed Ezzo <ahmed.ezzo@epfl.ch>
2020-03-25 18:36:55 +0200

    animated destroyable ships!! (test game)

--- mygame/game.js
+++ mygame/game.js
@@ -1,10 +1,17 @@
 window.onload = function(){
 
   var config = {
-    width: 800,
-    height: 600,
+    width: 256,
+    height: 272,
     backgroundColor: 0x000000,
-    scene: [Scene1, Scene2]
+    scene: [Scene1, Scene2],
+    pixelArt: true,
+    physics: {
+      default: "arcade",
+      arcade:{
+        debug: false
+      }
+    }
   }
   var game = new Phaser.Game(config);
 }
