48628a5b7d4a30d6bc39d2524325c874cc090edd
Aman Bansal <amanbansal3014@protonmail.com>
2020-03-26 15:11:12 +0100

    H, X, and CNOT transitions done

--- Server/Planet.py
+++ Server/Planet.py
@@ -10,7 +10,7 @@ class Planet:
         self.blueTransitions = None
         self.redTransitions = None
 
-    def set_transitions(self, h, x, cnot, color=None):
+    def set_transitions(self, h, x, cnot, color=None): # swap is mostly simple except for omega0 and omega3
         transitions = {EngineCard.H: h, EngineCard.X: x, EngineCard.CNOT: cnot}
         if color == Color.Blue:
             self.blueTransitions = transitions
@@ -19,6 +19,12 @@ class Planet:
         else:
             self.blueTransitions = transitions
             self.redTransitions = transitions
+
+    def get_transition(self, engineCard, color):
+        if color == Color.Blue:
+            return self.blueTransitions[engineCard]
+        else:
+            return self.redTransitions[engineCard]
 
 
 ONE = Planet("ONE", Galaxy.Centarious)
@@ -42,3 +48,24 @@ ZERO.set_transitions(PLUS, ONE, ONE)
 PLUS.set_transitions(ZERO, None, {ONE: PSI_PLUS, ZERO: PHI_PLUS})
 MINUS.set_transitions(ONE, None, {ONE: PSI_MINUS, ZERO: PHI_MINUS})
 
+PSI_PLUS.set_transitions(OMEGA3, PHI_PLUS, None, Color.Blue)
+PSI_PLUS.set_transitions(OMEGA0, PHI_PLUS, None, Color.Red)
+
+PSI_MINUS.set_transitions(OMEGA1, PHI_MINUS, None)
+
+PHI_PLUS.set_transitions(OMEGA2, PSI_PLUS, None)
+
+PHI_MINUS.set_transitions(OMEGA0, PSI_MINUS, None, Color.Blue)
+PHI_MINUS.set_transitions(OMEGA3, PSI_MINUS, None, Color.Red)
+
+OMEGA0.set_transitions(PHI_MINUS, OMEGA2, OMEGA2, Color.Blue)
+OMEGA0.set_transitions(PSI_PLUS, OMEGA1, None, Color.Red)
+
+OMEGA1.set_transitions(PSI_MINUS, OMEGA3, None, Color.Blue)
+OMEGA1.set_transitions(PSI_MINUS, OMEGA0, None, Color.Red)
+
+OMEGA2.set_transitions(PHI_PLUS, OMEGA0, OMEGA0, Color.Blue)
+OMEGA2.set_transitions(PHI_PLUS, OMEGA3, OMEGA3, Color.Red)
+
+OMEGA3.set_transitions(PSI_PLUS, OMEGA1, None, Color.Blue)
+OMEGA3.set_transitions(PHI_MINUS, OMEGA2, OMEGA2, Color.Red)
--- Server/Transitions.py
+++ Server/Transitions.py
@@ -2,8 +2,20 @@ from Enums import *
 import Planet
 
 
-def transition(color: Color, blue: Planet, red: Planet, card: EngineCard):
-    if color == Color.Blue:
-        
-    else
+def transition(player: Color, blue: Planet, red: Planet, engineCard: EngineCard):
+    if engineCard == EngineCard.SWAP or engineCard == EngineCard.PROBE:
+        raise Exception("Handle SWAP and PROBE on your own! They aren't worth my time")
 
+    # For now, the comment "CNOT: flip your ship only if the other ship is on CENTARIOUS ONE" was ignored
+    if player == Color.Blue:
+        g = blue.get_transition(engineCard, player)
+        if isinstance(g, Planet):
+            return g
+        else:
+            return g(red)
+    else:
+        g = red.get_transition(engineCard, player)
+        if isinstance(g, Planet):
+            return g
+        else:
+            return g(blue)
