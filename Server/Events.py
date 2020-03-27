496619105b935dbd442f0440f9c4667212c11876
Aman Bansal <amanbansal3014@protonmail.com>
2020-03-27 13:04:25 +0100

    Fixed event deck

--- Server/Events.py
+++ Server/Events.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from random import random
 
 
 class Event(Enum):
@@ -11,7 +12,8 @@ class Event(Enum):
     Schrodinger = -2
     Spooky = -3
     Collapse = -4
-    Shuffle = -5
+
+    # Shuffle = -5 doesn't need to be represented
 
     def can_save_for_later(self):
         return self.value > 0
@@ -19,4 +21,19 @@ class Event(Enum):
 
 class EventStack:
     def __init__(self):
+        self.deck = {}
+        self.initialize()
+
+    def initialize(self):
         self.deck = {e for e in Event}
+        for i in range(3):
+            del self.deck[random.choice(list(self.deck))]
+
+    def draw(self):
+        if len(self.deck) == 0:
+            self.initialize()
+            return None
+        else:
+            drawn = random.choice(list(self.deck))
+            del self.deck[drawn]
+            return drawn
