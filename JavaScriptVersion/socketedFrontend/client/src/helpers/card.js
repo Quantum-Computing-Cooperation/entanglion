e2adfa89b433c5849e18b048e390dda98b2b22e1
Ahmed Ezzo <ahmed.ezzo@epfl.ch>
2020-11-05 17:36:05 +0100

    removed js

--- JavaScriptVersion/socketedFrontend/client/src/helpers/card.js
+++
@@ -1,9 +0,0 @@
-export default class Card {
-    constructor(scene) {
-        this.render = (x, y, sprite) => {
-            let card = scene.add.image(x, y, sprite).setScale(0.3, 0.3).setInteractive();
-            scene.input.setDraggable(card);
-            return card;
-        }
-    }
-}
